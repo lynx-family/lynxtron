05950feed25e7b29a338feee04c25fba3e04d5a4
lidadating <springrain.ting@qq.com>
2026-02-03 15:06:40 +0800

    [Infra] lynxtron publish script
    Support lynxtron publish

--- packages/lynxtron-shell-demo/lynx.config.ts
+++ packages/lynxtron-shell-demo/lynx.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from '@lynx-js/rspeedy'
+import { defineConfig } from '@lynx-js/rspeedy';
 
-import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
-import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
-import { pluginTypeCheck } from '@rsbuild/plugin-type-check'
-import { pluginDevReady } from '@lynx-js/dev-ready-rsbuild-plugin'
-import { fileURLToPath } from "url";
-import path from "path";
+import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin';
+import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
+import { pluginDevReady } from '@lynx-js/dev-ready-rsbuild-plugin';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +12,10 @@ const rootPath = process.cwd();
 
 export default defineConfig({
   output: {
-    assetPrefix: `file://${path.resolve(__dirname, "./dist/assets/")}/`,
+    filename: {
+      bundle: 'main.lynx.bundle',
+    },
+    assetPrefix: `file://${path.resolve(__dirname, './dist/assets/')}/`,
     distPath: {
       root: './output/bundle',
     },
@@ -23,18 +25,19 @@ export default defineConfig({
       main: './src/app/index.tsx',
     },
     alias: {
-      "@assets": path.resolve(rootPath, "./src/assets"),
-    }
+      '@assets': path.resolve(rootPath, './src/assets'),
+    },
   },
   plugins: [
     pluginQRCode({
       schema(url) {
         // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
-        return `${url}`
+        return `${url}`;
       },
     }),
     pluginReactLynx(),
-    pluginTypeCheck(),
-    pluginDevReady({ markerFile: path.resolve(__dirname, './output/bundle/dev-ready.json') }),
+    pluginDevReady({
+      markerFile: path.resolve(__dirname, './output/bundle/dev-ready.json'),
+    }),
   ],
-})
+});
